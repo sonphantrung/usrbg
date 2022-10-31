5bbe4615f55f1cea880cb587b8e3ac929f82f71e
Son Phan Trung <phantrungson17@gmail.com>
2022-10-31 17:42:27 +0700

    yey, moved to css

---
+++ dist/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+uidandimg=$(curl -Ls https://raw.githubusercontent.com/Discord-Custom-Covers/usrbg/master/dist/usrbg.json | jq -r '.[] | "\(.img) \(.uid)"')
+
+rm -f usrbgnew.css
+
+while IFS=' ' read -r img uid; do
+	echo ".id-$uid{--u:url($img)}" >> usrbgnew.css
+done <<<$(echo "$uidandimg")
